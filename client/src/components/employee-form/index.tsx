2271872014ccca5c4fd520bfd5b4c27932921856
Nicelx <relush321@gmail.com>
2024-02-17 23:53:35 +0300

    small daily, employee form

--- client/src/components/employee-form/index.tsx
+++ client/src/components/employee-form/index.tsx
@@ -1,5 +1,7 @@
 import { Employee } from '@prisma/client';
+import { Card, Form } from 'antd';
 import React from 'react'
+import { CustomInput } from '../custom-input';
 type Props<T> = {
 	onFinish: (values: T) => void;
 	btnText: string;
@@ -11,8 +13,19 @@ type Props<T> = {
 export const EmployeeForm = ({
 	onFinish,
 	title,
+	btnText,
+	error,
+	employee,
 }: Props<Employee>) => {
   return (
-	<div>EmployeeForm</div>
+	<Card title = {title} style = {{width: '30rem'}}>
+		<Form name = "employee-form" onFinish = {onFinish} initialValues={employee}>
+			<CustomInput type = "text" name = "firstName" placeholder='Имя'/>
+			<CustomInput type = "text" name = "lastName" placeholder='Фамилия'/>
+			<CustomInput type = "number" name = "age" placeholder='age'/>
+			<CustomInput type = "text" name = "address" placeholder='address'/>
+			<Space></Space>
+		</Form>
+	</Card>
   )
 }
